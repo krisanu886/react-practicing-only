5311383f4963c740538f19c0c9cc70f9e03fc5d0
Krisanu <kisanudey886@gmail.com>
2022-01-03 23:24:09 +0530

    practicing-only-react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const myName ='Krisanu Dey';
+// const fName = 'Krisanu';
+// const lName = 'Dey';
+
+// const currentDate1 = new Date().toLocaleDateString();
+// const currentDate = new Date();
+// const day = currentDate.getDate();
+// const month = currentDate.getMonth()+1;
+// const year = currentDate.getFullYear();
+
+// const currentTime1 = new Date().toLocaleTimeString();
+// const currentTime = new Date();
+// const time = currentTime.getHours() + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds();
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
+    {/* 12 no video ------------------------- */}
+    {/* <h1>Hey buddy</h1>
+    
+    <p>My name is {myName}</p>
+    <ol>
+      <li>Dabang</li>
+      <li>Krish3</li>
+      <li>Bahubali</li>
+      <li>Ginius</li>
+    </ol> */}
+    {/* 12 no video -------------- */}
+
+    {/* 13 no video -------------- */}
+    {/* <h1>{`My first name is ${fName} and my last name is ${lName} and my lucky number is ${3*3}....`}</h1>
+    <p>my lucky number is {3*3}</p> */}
+    {/* 13 no video -------------- */}
+
+    {/* 14 no video -------------- */}
+    {/* <h1>Hello today i'll show current Date and Times</h1>
+    <p>{`Todays date is ${day} / ${month} / ${year}`}</p>
+    <p>{`Todays date is ${currentDate1}`}</p>
+    <p>{`And current time is ${time}`}</p>
+    <p>{`And current time is ${currentTime1}`}</p> */}
+    {/* 14 no video -------------- */}
+
+    {/* 15 no video -------------- */}
+    <h1 contentEditable='true'>Hey, my name is {myName}</h1>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
