b0e71400e3d01ede65d281efca13b461aec679ac
Krisanu <kisanudey886@gmail.com>
2022-01-04 19:12:26 +0530

    update-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const Increment = () =>{
+    setCount(count+1);
+  };
+
   return (
     <>
-      
+      <h1>{count}</h1>
+      <button onClick={Increment}>Increment</button>
     </>
   );
 }
-
 export default App;
